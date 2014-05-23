30921d9fe37090b0347c653e538abf4be163de70
HydraOrc <hydra0@bigmir.net>
2014-05-23 16:13:09 +0300

    ask questions and create passport

--- app/index.js
+++ app/index.js
@@ -1,54 +1,91 @@
-'use strict';
-var util = require('util');
-var path = require('path');
-var yeoman = require('yeoman-generator');
-var yosay = require('yosay');
-var chalk = require('chalk');
+var path, yeoman, yosay, PassportAuthGenerator;
 
+path = require('path');
+yeoman = require('yeoman-generator');
+yosay = require('yosay');
 
-var PassportAuthGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = require('../package.json');
-
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies();
-      }
-    });
-  },
-
-  askFor: function () {
-    var done = this.async();
-
-    // Have Yeoman greet the user.
-    this.log(yosay('Welcome to the marvelous PassportAuth generator!'));
-
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
-
-      done();
-    }.bind(this));
-  },
-
-  app: function () {
-    this.mkdir('app');
-    this.mkdir('app/templates');
-
-    this.copy('_package.json', 'package.json');
-    this.copy('_bower.json', 'bower.json');
-  },
-
-  projectfiles: function () {
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('jshintrc', '.jshintrc');
-  }
+PassportAuthGenerator = yeoman.generators.Base.extend({
+	init: function () {
+		
+	},
+	
+	askFor: function() {
+		var	done = this.async();
+		
+		this.log(yosay('Welcome to MongooseModel generator!'));
+		
+		var prompts = [
+			{
+				name: 'userModel',
+				message: 'What is the name of your user model?',
+				default: 'user'
+			},
+			
+			{
+				name: 'usernameField',
+				message: 'What is the name of your username field?',
+				default: 'username'
+			},
+			
+			{
+				name: 'passwordField',
+				message: 'What is the name of your password field?',
+				default: 'password'
+			},
+			
+			{
+				name: 'incorrectUsername',
+				message: 'What is your message in case of incorrect username?',
+				default: 'Incorrect username!'
+			},
+			
+			{
+				name: 'incorrectPassword',
+				message: 'What is your message in case of incorrect password?',
+				default: 'Incorrect password!'
+			},
+			
+			{
+				type: 'confirm',
+				name: 'ok',
+				message: 'Are you sure that everything is ok' +
+					' and you want to create passport?',
+				default: true
+			}
+		];
+		
+		this.prompt(prompts, function(props) {
+			if(props.ok) {
+				this.userModel = props.userModel.toLowerCase();
+				this.usernameField = props.usernameField.toLowerCase();
+				this.passwordField = props.passwordField.toLowerCase();
+				this.incorrectUsername = props.incorrectUsername;
+				this.incorrectPassword = props.incorrectPassword;
+				
+				done();
+			} else {
+				this.askFor();
+			}
+		}.bind(this));
+	},
+	
+	addPassport: function() {
+		var options = {
+			userModel: this.userModel,
+			usernameField: this.usernameField,
+			passwordField: this.passwordField,
+			incorrectUsername: this.incorrectUsername,
+			incorrectPassword: this.incorrectPassword
+		},
+		fileName = 'passport.coffee';
+		
+		this.sourceRoot(path.join(__dirname, '/templates'));
+		this.template(
+			fileName,
+			fileName,
+			options
+		);
+	}
 });
 
 module.exports = PassportAuthGenerator;
---
+++ app/templates/passport.coffee
@@ -0,0 +1,75 @@
+passport = require 'passport'
+passport_local = require 'passport-local'
+LocalStrategy = passport_local.Strategy
+async = require 'async'
+
+user = require '<%= userModel %>'
+
+parameters =
+	usernameField: '<%= usernameField %>'
+	passwordField: '<%= passwordField %>'
+
+messages =
+	incorrectUsername: '<%= incorrectUsername %>'
+	incorrectPassword: '<%= incorrectPassword %>'
+
+passport.serializeUser (user, done) ->
+	done null, user.id
+
+passport.deserializeUser (id, done) ->
+	user.model.findOne
+		_id: id
+	, (err, user) ->
+		done err, user
+
+local = new LocalStrategy parameters, (username, password, done) ->
+	user.model.findOne
+		$or: [
+			{
+				email: username
+			}
+			{
+				username: username
+			}
+		]
+	, (err, usr) ->
+		return done(err) if err
+		unless usr
+			return done null, false, 
+				message: messages.incorrectUsername
+		unless usr.validPassword password
+			return done null, false, 
+				message: messages.incorrectPassword
+		done null, usr
+
+passport.use local
+
+module.exports =
+	passport: passport
+	register: (req, res) ->
+		usr = new user.model 
+			username: req.body.username
+			email: req.body.email
+			password: req.body.password
+		
+		async.waterfall [
+			(next) ->
+				usr.save next
+			->
+				res.redirect "/"
+		], (err) ->
+			console.log err
+			res.redirect "/register"
+	
+	login: passport.authenticate "local", 
+		failureRedirect: "/login"
+	
+	logout: (req, res) ->
+		req.logout
+		res.redirect "/"
+	
+	logged_in: (req, res, next) -> 
+		if req.isAuthenticated then next else res.redirect "/"
+	
+	not_logged_in: (req, res, next) -> 
+		if not req.isAuthenticated then next else res.redirect "/"
